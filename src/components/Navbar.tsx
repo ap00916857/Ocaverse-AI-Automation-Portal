e3d91aa9b07f16cce86e8560ee24b0d169ae36ed
ap00916857 <ap00916857@gmail.com>
2026-05-12 12:30:48 +0530

    fix3

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -37,7 +37,7 @@ export const Navbar = () => {
             <img
               src={logo}
               alt="OcaVerse logo"
-              className="h-[95px] md:h-[120px] w-auto object-contain select-none"
+              className="h-[75px] md:h-[200px] w-auto object-contain select-none"
               draggable={false}
             />
           </a>
